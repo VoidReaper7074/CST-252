1db5f4bb1639b998a3fb051f9ed67e07a8716314
VoidReaper7074 <60455567+VoidReaper7074@users.noreply.github.com>
2022-02-09 03:43:52 -0800

    lab4

---
+++ lab4/js/lab.js
@@ -0,0 +1,116 @@
+//Author: Douglas Pattison
+//Summary: 9 steps of making a D&D character.
+
+//Creating a D&D 5e Character for Beginners!
+//Step 1: Choose Race, Class, and Background
+  //Prior to filling out your character sheet, there are three things you should decide. You need to decide what race and class your character is going to be, as well as their background.
+    //Choose a Race
+      //Race is your character's species. Your character's race helps determine your physical look as well as giving you natural talents. Racial traits include the following: Ability Score Increase, Age, Alignment, Size, Speed, Languages, and Subraces. You can read the first few paragraphs of each race's section to get an idea of what each of them are.
+    //Choose a Class
+      //Class is your character's profession, and determines what actions your character can perform. Your character gains some special class features and proficiencies centered around their chosen vocation. You can read the first few paragraphs of each class's section to get an idea of what each of them are.
+    //Choose Background
+      //Your character's background is simply your characters history. Where do they come from and what were they doing prior to the game you are about to play. They provide additional proficiencies, languages, and skills. You can read the first few paragraphs of each background's section to get an idea of what each of them are.
+
+
+//Step 2: Stat Blocks
+  // Ability Scores:
+    //Ability Scores are determined via two methods. You may roll dice to determine which numbers you have to work with, you can choose to use the default numbers. Also, your race applies some benefits to certain ability scores.
+  //Ability Modifier:
+    //The Ability Modifier is the amount you would add to the dice roll as you are playing the game. If your modifier is a +2, and you rolled a 12, you final score for that check is 14. The higher your result, the better, and the number you are trying to beat is determined by your DM based on the difficulty of the task.
+  //Inspiration:
+    //Inspiration is not used in character creation. Your character may earn inspiration from other players or the DM in game, and is tracked here.
+  //Proficiency Modifier:
+    //Proficiency Modifier is a bonus you add to any checks made with a skill or weapon you are proficient with. All characters start with a +2 proficiency modifier at level 1. This will increase as a character levels up. Reference your class section to see at what levels this increases.
+  //Saving Throws:
+    //Saving throws are ability checks that you make when someone else is targeting you, such as a spell that you are trying to resist.
+  //Skill Scores:
+    //Skill scores are ability checks that apply to specific scenarios. If you are trying to bluff someone, you would use Deception. If you are trying to tell if someone is bluffing, you would use Insight. These scores are added to any rolls you make for one of these scores.
+  //Passive Perception:
+    //Passive Wisdom is your characters natural awareness of their surroundings. When you are not actively looking for something, this score determines what you see without needing to make a check.
+
+
+//Step 3: Proficiencies and Languages
+  // Proficiencies:
+    //Non-skill proficiencies are skills or items that you character can use without any penalties.
+  //Languages:
+    //Check BOTH your race and background for any known languages.
+
+//Step 4: Equipment
+  //Your equipment consists of all the items that they are carrying. This includes their armor, weapons, traveling gear, exploring gear, etc.
+
+//Step 5: Attacks and Spellcasting
+  //Physical Weapons:
+    //Physical weapons are those such as staffs, swords, and bows.
+  //Attack Modifier:
+    //Attack modifier is the score you add to a d20 roll when attempting to attack with that weapon. You compare the final number to the targets AC, and successfully hit if your number is equal to, or greater than, their AC.
+  //Damage:
+    //Damage is the amount of damage you do to the target on a successful hit. To determine the damage dice and type
+
+
+//Step 6: HP and Combat Stats
+  //Armor Class:
+    //Determined by the armor that you are wearing and/or your dexterity modifier. Your Armor Class, or AC, determines how hard you are to hit in combat.
+  // Initiative:
+    //Initiative is your ability to act quickly in the face of danger. When you roll for Initiative, you roll a d20 and add your initiative modifier.
+  //Speed:
+    //Speed is how far you can move with a single movement action.
+  //Hit Dice:
+    //Hit dice determine how hearty you are, and your ability to heal when resting.
+  // Hit Point Maximum:
+    //Your hit point maximum is the most HPs you can have.
+  //Current Hit Points:
+    //Current hit points is the amount of health you have remaining. This number can not go above your hit point maximum. You may use this block as you play the game to keep track of damage you have taken. If your current hit points ever reach 0, you become unconscious.
+  //Temporary Hit Points:
+    //Temporary Hit Points are not used in character creation. Spells and other buffs give you temporary hit points. These can not be healed, and act more of as a buffer for your current hit points.
+  //Death Saves:
+    //Death Saves are not used in character creation. The are used when characters reach 0 HP when playing the game, and are the bases of determining if a character is just knocked out or is actually dead.
+
+//Step 7: Features
+    //Features:
+      //The features block is a place to list all remaining features of your class, race, and background. Any additional skills, passive benefits, or relevant bonuses from background can be listed here.
+
+
+//Step 8: Traits
+  //Personality traits:
+    //General descriptions about your character, that help differentiate different characters from each other.
+  //Ideals:
+    //Ideals are the things that your character believes strongly in.
+  //Bonds:
+    //How your character is tied to the world of the game. Can be a person, place or event.
+  //Flaws:
+    //A vice, compulsion, fear, or weakness.
+
+//Step 9: Name and Remaining Information
+  // Character Name:
+  // Class and level:
+    //A place to track your class and level.
+  //Background:
+    //A place to write your chosen background.
+  //Player name:
+    //A place to write your own name.
+  // Race:
+    //A place to write your chosen race.
+  //Alignment:
+    //Alignment is your general temperament to others and the world around you.
+
+
+//CODE 2 STARTS HERE//
+
+
+//Homepage Video Intro
+
+//When page is opened
+  //show black screen with playbutton icon centered
+    //when clicked: start step 2
+//Step2: rectanlge appears from left of screen
+  //animation of rectangle going to center of screen
+    //When rectangle hovered, play button reappears
+      //when play button clicked: start step 3
+//Step3: Play button (vector graphic acting as hyperlink) when clicked:
+  //video player appears in rectangle
+    // after 3 seconds: start introduction to site Video
+      //after video ends start step 4
+//Step4: video player disappears
+  //show enter site button in its place
+    //when clicked:
+      //load homepage to website
