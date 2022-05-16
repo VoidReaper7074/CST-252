390f996f6eefb8006cbd6fbb0445f0f547919056
VoidReaper7074 <60455567+VoidReaper7074@users.noreply.github.com>
2022-05-15 22:55:47 -0700

    resubmit

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -1,10 +1,18 @@
-$("#challenge-btn").click(function (){
-	$("#challenges").toggleClass("pinktext");
+/*
+* lab11\2.js - Advanced jQUery
+* Author: Raeann Anaya raanaya@csumb.edu>
+* Created: 13 October
+* License: Public Domain
+*/
+
+$("#btn-red").click(function() {
+  $("#content").toggleClass("red-text")
 });
 
-$("#problems-btn").click(function (){
-	$("#problems").toggleClass("big");
+$("#btn-green").click(function(){
+  $("#content").toggleClass("green-text")
 });
 
-$("#results-btn").click(function (){
-	$("#results").toggleClass("highlight");
+$("#btn-blue").click(function(){
+  $("#content").toggleClass("blue-text")
+});
